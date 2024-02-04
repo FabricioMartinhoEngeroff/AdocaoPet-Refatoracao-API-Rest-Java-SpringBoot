128f64c1afd4ebaf3c8a1ad84823ba19c3df576a
Fabricio <fa.engeroff1996@gmail.com>
2024-02-04 08:22:37 -0300

    Finalizando ma Etapa da Refatoração

---
+++ src/main/java/br/com/alura/CommandExecutor.java
@@ -0,0 +1,11 @@
+package br.com.alura;
+
+
+import br.com.alura.service.Command;
+
+public class CommandExecutor {
+
+    public void CommandExecute(Command command){
+          command.execute();
+    }
+}
